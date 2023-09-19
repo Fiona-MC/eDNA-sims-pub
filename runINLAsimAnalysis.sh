06a324d6a8f7e2efaac7f64d56d2b78bc728de7d
Fiona Callahan <fiona_callahan@berkeley.edu>
2023-09-19 09:24:11 -0700

    running for code from paper

--- runINLAsimAnalysis.sh
+++ runINLAsimAnalysis.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 export OMP_NUM_THREADS=15
 
-sim_dir="/space/s1/fiona_callahan/multiSim_rw"
+sim_dir="/space/s1/fiona_callahan/multiSim_rw3"
 numRuns=100
 numTrials=1
+INLA_type="paper"
+#INLA_type="faster"
 
 Rscript /home/fiona_callahan/eDNA_sims_code/filter_sims.R ${sim_dir}/ ${numRuns}
 # Rscript /home/fiona_callahan/filter_sims.R /space/s1/fiona_callahan/multiSim3/
@@ -23,13 +25,13 @@ N=10 # N=10 resulted in average usage around 30 cores
 
 for folder in ${sim_dir}/randomRun*; do
     (
-        if test ! -d "${folder}/INLA_res_faster/trial1" # if the folder is not already there NOT WORKING
+        if test ! -d "${folder}/INLA_res_${INLA_type}/trial1" # if the folder is not already there NOT WORKING
         then
             echo "starting task $folder.."
-            mkdir "$folder/INLA_res_faster/" 
+            mkdir "$folder/INLA_res_${INLA_type}/" 
             # run INLA sim analysis
-            timeout -k 10 2h Rscript /home/fiona_callahan/eDNA_sims_code/INLA_simAnalysis_faster.R ${folder}/ ${folder}/INLA_res_faster/
-            Rscript /home/fiona_callahan/eDNA_sims_code/count_INLAmistakes.R ${folder}/ ${folder}/INLA_res_faster/
+            timeout -k 10 2h Rscript /home/fiona_callahan/eDNA_sims_code/INLA_simAnalysis_${INLA_type}.R ${folder}/ ${folder}/INLA_res_${INLA_type}/
+            Rscript /home/fiona_callahan/eDNA_sims_code/count_INLAmistakes.R ${folder}/ ${folder}/INLA_res_${INLA_type}/
             sleep $(( (RANDOM % 3) + 1)) # choose random number 1, 2, or 3 and sleep for that long -- no idea why
         fi
     ) &
