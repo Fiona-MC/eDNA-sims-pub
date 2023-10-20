1e5f5e627899ab355c045db337714279573a9d63
Fiona Callahan <fiona_callahan@berkeley.edu>
2023-10-20 15:15:35 -0700

    generate parms for many species, including abundance

--- multiSimParms.R
+++ multiSimParms.R
@@ -15,17 +15,17 @@ getParms_json <- function(parmFile = "parameters.json") { #TODO this is not impl
 }
 
 
-getParms_many <- function(random = FALSE, parmSet = 1) {
+getParms_many <- function(random = FALSE, parmSet = 1, numSpecies = 100) {
     if (random == TRUE) {
         num_samples_time <- sample(x = 5:50, size = 1) # sample times per location
         num_samples_space <- sample(5:50, size = 1) # sample locations per time
         #radius <- 16
         radius <- runif(n = 1, min = 12, max = 50) # for neighborhoods: a little more the 
         # hypotenuse of 11.1 unit grid (16), to get neighbors be "right" for this setup
-        mean_fpr <- runif(n = 1, min = 0, max = 0.2)
+        #mean_fpr <- runif(n = 1, min = 0, max = 0.2)
         #fpr_mode <- sample(x = c("independent", "dependent_sp"), size = 1)
 
-        fpr_mode <- sample(x = c("none", "independent", "dependent_sp", "constant"), size = 1)
+        #fpr_mode <- sample(x = c("none", "independent", "dependent_sp", "constant"), size = 1)
         #fpr_mode <- "independent"
         covNoise_sd <- runif(n = 1, min = 0, max = 0.1) # process noise
         covMeasureNoise_sd <- runif(n = 1, min = 0, max = 0.2) # measurement noise
@@ -36,39 +36,43 @@ getParms_many <- function(random = FALSE, parmSet = 1) {
         # noise param for popn growth 
         sigma <- runif(n = 1, min = 0, max = 0.2)
 
-        N_50 <- sample(x = 1:1000, size = 1) # number of animals for which detection prob is 50%
+        #N_50 <- sample(x = 1:1000, size = 1) # number of animals for which detection prob is 50%
 
         #species effect
         c2 <- runif(n = 1, min = 100, max = 200)
 
         mean_mig_rate <- runif(n = 1, min = 0, max = 0.1) # poisson rate per individual per time
 
-        N0_0 <- 10
-        x_split <- 8
-        y_split <- 8
+        indivSampleProb <- runif(n = 1, 0.001, 0.02)
+        readSampleRate <- runif(n = 1, 0.5, 100)
+        corr_mode <- sample(c("independent")) # this is the only one implemented thus far
     } else { 
         num_samples_time <- 50 # sample times per location
         num_samples_space <- 50 # sample locations per time
         radius <- 16 # for neighborhoods: a little more the 
         # hypotenuse of 11.1 unit grid, to get neighbors be right for this setup
-        mean_fpr <- 0.05
-        fpr_mode <- "none" # "dependent_sp" "constant" "none"
+        #mean_fpr <- 0.05
+        #fpr_mode <- "none" # "dependent_sp" "constant" "none"
         covNoise_sd <- 0
         #intrinsic growth rate
         r <- 0.05
         #noise param for popn growth 
         sigma <- 0.005
 
-        N_50 <- 50 # number of animals for which detection prob is 50%
+        #N_50 <- 50 # number of animals for which detection prob is 50%
 
         mean_mig_rate <- 0.01
 
         N0_0 <- 10
-        x_split <- 8
-        y_split <- 8
+        x_split <- 10
+        y_split <- 10
 
         c2 <- 300 #changed to be varied -- sp effect
         covMeasureNoise_sd <- 0
+
+        indivSampleProb <- 0.01
+        readSampleRate <- 1
+        corr_mode <- "independent"
     }
 
     #TODO change to accommodate actual spatial locations -- this is mostly done I think -- 
@@ -77,28 +81,24 @@ getParms_many <- function(random = FALSE, parmSet = 1) {
     ydim1 <- 100
     location_mode <- "grid"
     
-    constant_fpr <- 0.05
-    beta_fpr <- 50 # mean = alpha/(alpha+beta) = 5/(100+5) 
-    alpha_fpr <- (-beta_fpr * mean_fpr) / (mean_fpr - 1) # moment matching
+    #constant_fpr <- 0.05
+    #beta_fpr <- 50 # mean = alpha/(alpha+beta) = 5/(100+5) 
+    #alpha_fpr <- (-beta_fpr * mean_fpr) / (mean_fpr - 1) # moment matching
     #fpr_mode = "dependent_sp" # "independent" "dependent_sp" "constant" "none"
 
     num_gens <- 1000
     #detection prob param
 
-    det_prob_exp <- 2 # fix this at 2
-    det_prob_add <- N_50^det_prob_exp # vary this 
-
-
+    #det_prob_exp <- 2 # fix this at 2
+    #det_prob_add <- N_50^det_prob_exp # vary this 
 
     abundanceEffectType <- "arctangent"
 
     covVars <- list()
-    covVars[[1]] <- list(type = "randomWalk")
-    covVars[[2]] <- list(type = "randomWalk")
-    covVars[[3]] <- list(type = "randomWalk")
-    covVars[[4]] <- list(type = "randomWalk")
-    covVars[[5]] <- list(type = "randomWalk")
-    covVars[[6]] <- list(type = "constant")
+    for (i in 1:numSpecies) {
+      covVars[[i]] <- list(type = "randomWalk")
+    }
+    covVars[[numSpecies + 1]] <- list(type = "constant")
 
 
     numCovs <- length(covVars)
@@ -107,28 +107,29 @@ getParms_many <- function(random = FALSE, parmSet = 1) {
                       return(paste0("Cov", cov))}}) 
     names_cov <- unlist(names_cov)
     names_all_cov <- unlist(sapply(1:numCovs, FUN = function(cov) {paste0("Cov", cov)}))
-    numSpecies <- 5
     names_species <- sapply(1:numSpecies, FUN = function(sp) {paste0("Sp", sp)}) # nolint: brace_linter.
 
-    #TODO
     alpha <- matrix(0, nrow = numSpecies, ncol = numSpecies)
     beta <- matrix(0, nrow = numSpecies, ncol = numCovs)
+     
+    # choose species interactions
+    for(ii in sample(x = 1:(numSpecies %/% 2), size = numSpecies %/% 2, replace = TRUE)) {
+      for(jj in sample(x = 1:(numSpecies %/% 2), size = numSpecies %/% 2, replace = TRUE)) {
+        alpha[ii, jj] <- sample(x = c(-1, 1), size = 1)
+      }
+    }
 
-    alpha[1, 2] <- -1
-    alpha[2, 1] <- -1
-    alpha[5, 3] <- -1
-    alpha[5, 4] <- -1
+    for(ii in sample(x = (numSpecies %/% 2 + 1):numSpecies, size = numSpecies %/% 2, replace = TRUE)) {
+      for(jj in sample(x = (numSpecies %/% 2 + 1):numSpecies, size = numSpecies %/% 2, replace = TRUE)) {
+        alpha[ii, jj] <- sample(x = c(-1, 1), size = 1)
+      }
+    }
 
-    beta[1, 1] <- 1
-    beta[2, 2]  <- 1
-    beta[3, 3]  <- 1
-    beta[4, 4]  <- 1
-    beta[5, 5]  <- 1
-    beta[1, 6] <- 1
-    beta[2, 6] <- 1
-    beta[3, 6] <- 1
-    beta[4, 6] <- 1
-    beta[5, 6] <- 1
+    # set environment interactions as independent
+    for(i in 1:numSpecies) {
+      beta[i, i] <- 1 # one independent covariate per species
+      beta[i, numSpecies + 1] <- 1 # this is the intercept variable
+    }
 
     #alpha and beta numbers respectively (for scaling purposes)
     #cov effect
@@ -140,6 +141,16 @@ getParms_many <- function(random = FALSE, parmSet = 1) {
     #mig_rates=rep(0.1, times=numSpecies) # OR vvv sample this from gamma per species
     mig_rates <- rgamma(n = numSpecies, shape = mean_mig_rate / 0.01, scale = 0.01) # mean = 0.1, mode 0.09, variance 0.001
 
+    # parameters from old versions that dont exist in abundance version anymore
+    det_prob_exp <- NA
+    det_prob_add <- NA
+    alpha_fpr <- NA
+    beta_fpr <- NA
+    constant_fpr <- NA
+    N_50 <- NA
+    mean_fpr <- NA
+    fpr_mode <- NA
+
     params <- list(xdim = xdim1, ydim = ydim1, location_mode = location_mode, x_split = x_split, y_split = y_split, 
                num_gens = num_gens, mig_rates = mig_rates, mean_mig_rate = mean_mig_rate, 
                numCovs = numCovs, numSpecies = numSpecies, r = r, sigma = sigma, 
@@ -147,7 +158,8 @@ getParms_many <- function(random = FALSE, parmSet = 1) {
                abundanceEffectType = abundanceEffectType, beta = beta, alpha = alpha, fpr = list(mean_fpr = mean_fpr, mode = fpr_mode, 
                alpha_fpr = alpha_fpr, beta_fpr = beta_fpr, constant_fpr = constant_fpr), names_cov = names_cov, names_all_cov = names_all_cov,
                names_species = names_species, N0_0 = N0_0, covNoise_sd = covNoise_sd, covMeasureNoise_sd = covMeasureNoise_sd, 
-               num_samples_time = num_samples_time, num_samples_space = num_samples_space, radius = radius)
+               num_samples_time = num_samples_time, num_samples_space = num_samples_space, radius = radius, indivSampleProb = indivSampleProb, 
+               readSampleRate = readSampleRate, corr_mode = corr_mode)
     return(params)
 }
 
