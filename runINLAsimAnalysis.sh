680f4a6a9dd8d086348268a608761b7d9cfd5f6b
Fiona Callahan <fiona_callahan@berkeley.edu>
2023-12-11 13:40:22 -0800

    added cutoff stuff

--- runINLAsimAnalysis.sh
+++ runINLAsimAnalysis.sh
@@ -2,10 +2,14 @@
 export OMP_NUM_THREADS=5
 
 sim_dir="/space/s1/fiona_callahan/multiSim_100"
-numRuns=5
+numRuns=90
 numTrials=1
 INLA_type="paper"
+resDirName=INLA_res_${INLA_type}
 scramble=0
+covs=1
+
+
 #INLA_type="faster"
 
 #Rscript /home/fiona_callahan/eDNA_sims_code/filter_sims.R ${sim_dir}/ ${numRuns}
@@ -29,10 +33,15 @@ for folder in ${sim_dir}/randomRun*; do
         #if test ! -d "${folder}/INLA_res_${INLA_type}/trial1" # if the folder is not already there 
         #then
             echo "starting task $folder.."
-            mkdir "$folder/INLA_res_${INLA_type}/" 
+            #mkdir "$folder/INLA_res_${INLA_type}/" 
             # run INLA sim analysis
-            timeout -k 10 10h Rscript /home/fiona_callahan/eDNA_sims_code/INLA_simAnalysis_${INLA_type}.R ${folder}/ ${folder}/INLA_res_${INLA_type}/ $scramble
-            Rscript /home/fiona_callahan/eDNA_sims_code/count_INLAmistakes.R ${folder}/ ${folder}/INLA_res_${INLA_type}/
+            #timeout -k 10 10h Rscript /home/fiona_callahan/eDNA_sims_code/INLA_simAnalysis_${INLA_type}.R ${folder}/ ${folder}/INLA_res_${INLA_type}/ $scramble
+            for cutoff in 0 1 0.0000001 0.001 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1 0.15 .3 .5;
+            #for cutoff in 0 0.0000000000001 0.0000001 0.00001 .3 .5 .7 .9 1;
+            do
+                Rscript /home/fiona_callahan/eDNA_sims_code/INLA_changeCutoffs.R ${folder}/ ${cutoff}
+                Rscript /home/fiona_callahan/eDNA_sims_code/count_mistakes_general.R ${folder}/ ${folder}/${resDirName}/ ${covs} ${cutoff}
+            done
             sleep $(( (RANDOM % 3) + 1)) # choose random number 1, 2, or 3 and sleep for that long -- no idea why
        #fi
     ) &
@@ -49,6 +58,11 @@ done
 # (all need to be finished)
 wait
 
-Rscript /home/fiona_callahan/eDNA_sims_code/gather_inferenceRes.R ${sim_dir}/ ${numRuns} ${numTrials} ${scramble}
+#for cutoff in 0.001 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1 0.15;
+for cutoff in 0 1 0.0000001 0.001 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1 0.15 .3 .5;
+do
+Rscript /home/fiona_callahan/eDNA_sims_code/gather_inferenceRes_ecoCopula.R ${sim_dir}/ ${numRuns} ${numTrials} ${resDirName} ${cutoff}
+done
 
 echo "all done"
+
