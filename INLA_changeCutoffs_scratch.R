b42838286ba9f8f733c88ccf43ca1761c6c03a75
Fiona Callahan <fiona_callahan@berkeley.edu>
2023-12-19 09:36:00 -0800

    added ROC_mode to stop doing WAIC selection if we want

---
+++ INLA_changeCutoffs_scratch.R
@@ -0,0 +1,71 @@
+library(data.table)
+
+args <- commandArgs(trailingOnly = TRUE)
+
+sim_dir <- "/space/s1/fiona_callahan/multiSim_100/randomRun2/"
+cutoff <- 0.05
+save_dir <- "/space/s1/fiona_callahan/multiSim_100/randomRun2/INLA_res_faster/"
+
+sim_dir <- args[1]
+cutoff <- as.numeric(args[2])
+save_dir <- args[3]
+ROC_mode <- args[4] #"noModelSelect" or "modelSelect"
+
+
+trial <- 1
+subdir <- paste0(save_dir, "trial", trial, "/")
+params <- readRDS(paste0(sim_dir, "params.Rdata"))
+sim_lists <- readRDS(paste0(subdir, "sim_lists-", trial, ".Rdata"))
+
+modelcands <- names(sim_lists)
+
+names_species <- params$names_species
+names_cov <- params$names_cov
+
+# waic
+if (length(modelcands) > 1) {
+    waictab <- sapply(names_species, function(response) {
+        return(sapply(modelcands, function(modelname) {
+                    sim_lists[[modelname]][[response]]$waic$waic}))
+    })
+    rownames(waictab) <- modelcands
+}
+
+# get inferred alpha and beta (just 1's -1's and 0's )
+inferenceRes <- list()
+inferenceRes$alphaInferred <- matrix(data = 0, nrow = length(names_species), ncol = length(names_species))
+inferenceRes$betaInferred <- matrix(data = 0, nrow = length(names_species), ncol = params$numCovs)
+for (sp_index in seq_along(names_species)) {
+    if (ROC_mode == "noModelSelect") {
+        if (length(modelcands) > 1) {
+            this_best_model <- "Cov+Animal"
+        } else {
+            this_best_model <- modelcands[1]
+        }
+    } else {
+        if(length(modelcands) > 1) {
+            this_best_model <- modelcands[which.min(waictab[, sp_index])]
+        } else {
+            this_best_model <- modelcands[1]
+        }
+    }
+    best_model_res <- sim_lists[[this_best_model]][[names_species[sp_index]]]
+    #lower_bound <- best_model_res$summary.fixed$`0.025quant`
+    #upper_bound <- best_model_res$summary.fixed$`0.975quant`
+    z_score <- qnorm(1 - cutoff / 2)
+    lower_bound <- best_model_res$summary.fixed$mean - z_score * best_model_res$summary.fixed$sd
+    upper_bound <- best_model_res$summary.fixed$mean + z_score * best_model_res$summary.fixed$sd
+    significant_pos_vars <- row.names(best_model_res$summary.fixed)[lower_bound > 0]
+    significant_neg_vars <- row.names(best_model_res$summary.fixed)[upper_bound < 0]
+    for (sp_index2 in seq_along(names_species)) { # put 1's and -1's into alpha and beta inferred based on 95% CI
+    if (names_species[sp_index2] %in% significant_pos_vars) {inferenceRes$alphaInferred[sp_index, sp_index2] <- 1}
+    if (names_species[sp_index2] %in% significant_neg_vars) {inferenceRes$alphaInferred[sp_index, sp_index2] <- -1}
+    }
+    for (cov_index in seq_along(names_cov)) {
+    if (names_cov[cov_index] %in% significant_pos_vars) {inferenceRes$betaInferred[sp_index, cov_index] <- 1}
+    if (names_cov[cov_index] %in% significant_neg_vars) {inferenceRes$betaInferred[sp_index, cov_index] <- -1}
+    }
+}
+
+saveRDS(inferenceRes, paste0(subdir, "inferenceRes_cutoff", cutoff, ".Rdata"))
+
--- scratch.sh
+++ scratch.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 export OMP_NUM_THREADS=5
 
-sim_dir="/space/s1/fiona_callahan/multiSim_100"
-numRuns=100
+sim_dir="/space/s1/fiona_callahan/multiSim_test"
+numRuns=3
 numTrials=1
-INLA_type="faster"
+INLA_type="paper"
 resDirName=INLA_res_${INLA_type}
 scramble=0
 covs=1
 #sitetab="sim_sitetab_sampled.csv"
 sitetab="sim_sitetab_sampled.csv"
+ROC_mode="noModelSelect" # this will mean there is no WAIC selection for the ones where the cutoff changes
 
 #INLA_type="faster"
 
@@ -26,7 +27,7 @@ sitetab="sim_sitetab_sampled.csv"
 #done < ${sim_dir}/unrealistic_runNums.csv
 
 
-N=5 # N=10 resulted in average usage around 30 cores
+N=3 # N=10 resulted in average usage around 30 cores
 # based on current rate with N=10 -- this should take ~6 days for 1000 runs (2 trials each)
 
 for folder in ${sim_dir}/randomRun*; do
@@ -36,11 +37,12 @@ for folder in ${sim_dir}/randomRun*; do
             echo "starting task $folder.."
             mkdir "$folder/INLA_res_${INLA_type}/" 
             # run INLA sim analysis
-            timeout -k 10 10h Rscript /home/fiona_callahan/eDNA_sims_code/INLA_simAnalysis_${INLA_type}.R ${folder}/ ${folder}/INLA_res_${INLA_type}/ $sitetab
-            for cutoff in 0 1 0.0000001 0.001 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1 0.15 .3 .5;
+            timeout -k 10 10h Rscript /home/fiona_callahan/eDNA_sims_code/INLA_simAnalysis_${INLA_type}.R ${folder}/ ${folder}/${resDirName}/ $sitetab
+            #for cutoff in 0.01;
             #for cutoff in 0 0.0000000000001 0.0000001 0.00001 .3 .5 .7 .9 1;
+            for cutoff in 0 1 0.0000001 0.001 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1 0.15 .3 .5;
             do
-                Rscript /home/fiona_callahan/eDNA_sims_code/INLA_changeCutoffs.R ${folder}/ ${cutoff}
+                Rscript /home/fiona_callahan/eDNA_sims_code/INLA_changeCutoffs_scratch.R ${folder}/ ${cutoff} ${folder}/${resDirName}/
                 Rscript /home/fiona_callahan/eDNA_sims_code/count_mistakes_general.R ${folder}/ ${folder}/${resDirName}/ ${covs} ${cutoff}
             done
             sleep $(( (RANDOM % 3) + 1)) # choose random number 1, 2, or 3 and sleep for that long -- no idea why
@@ -60,6 +62,7 @@ done
 wait
 
 #for cutoff in 0.001 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1 0.15;
+#for cutoff in 0.01;
 for cutoff in 0 1 0.0000001 0.001 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1 0.15 .3 .5;
 do
 Rscript /home/fiona_callahan/eDNA_sims_code/gather_inferenceRes_ecoCopula.R ${sim_dir}/ ${numRuns} ${numTrials} ${resDirName} ${cutoff}
