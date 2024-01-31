2bd5df8fa873becdc99e24d631c4a5448a995367
Fiona Callahan <fiona_callahan@berkeley.edu>
2024-01-31 14:14:54 -0800

    change out name

--- runLogisticSimAnalysis.sh
+++ runLogisticSimAnalysis.sh
@@ -43,6 +43,8 @@ fi
 
 outname=${outname}_${numRuns}runs
 
+echo ${outname}
+
 Rscript /home/fiona_callahan/eDNA_sims_code/logisticFromSim_moreSp.R ${sim_dir}/ ${numRuns} ${covs} ${dumb} ${sitetab} ${outname}
 
 echo "all done"
